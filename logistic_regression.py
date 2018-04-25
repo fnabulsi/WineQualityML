e947257ec2b330f5ef8ef2789a341a3c2d95c983
Farris <fnabulsi7@gmail.com>
2018-04-25 00:02:16 -0700

    Fixed bugs for Logistic Regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -8,7 +8,8 @@ from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 from conf_matrix import func_confusion_matrix
 
-wine = pd.read_csv("/Users/ftnabulsi/cs596/WineQualityML/winequality-red.csv", delimiter=';')
+wine = pd.read_csv(
+    "/Users/ftnabulsi/cs596/WineQualityML/winequality-red.csv", delimiter=';')
 
 #Get labels and features
 wineLabels = wine['quality']    #Ground truth values
@@ -17,20 +18,22 @@ wineFeatures = wine.drop('quality', axis=1) #features (X)
 testSize = 0.20
 seed = 10
 
-#Split data into training and testing data
-featuresTrain, xTest, labelsTrain, yTest = model_selection.train_test_split(wineFeatures, wineLabels, test_size=testSize, random_state=seed)
+scaler = preprocessing.StandardScaler()
+scaler.fit(wineFeatures)
+wineFeatures = scaler.transform(wineFeatures)
 
-#Normalize the data
-normalizer = preprocessing.MinMaxScaler()
-featuresTrain = normalizer.fit_transform(featuresTrain)
-xTest = normalizer.fit_transform(xTest)
+#Split data into training and testing data
+featuresTrain, xTest, labelsTrain, yTest = model_selection.train_test_split(
+    wineFeatures, wineLabels, test_size=testSize, random_state=seed)
 
 #Split training data into training and validation
-xTrain, xValidation, yTrain, yValidation = model_selection.train_test_split(featuresTrain, labelsTrain, test_size=testSize, random_state=seed)
+xTrain, xValidation, yTrain, yValidation = model_selection.train_test_split(
+    featuresTrain, labelsTrain, test_size=testSize, random_state=seed)
 
 #Run the model
 MAX_ITERATIONS = 100
-logReg = linear_model.LogisticRegression(max_iter=MAX_ITERATIONS)
+logReg = linear_model.LogisticRegression(
+    max_iter=MAX_ITERATIONS, C=10, random_state=seed)
 logReg.fit(xTrain, yTrain)
 error = 1 - logReg.score(xValidation, yValidation)
 
@@ -44,4 +47,25 @@ cMatrix = confusion_matrix(yTest, yPred, labels=wineLabels)
 print("Accuracy on test data is:", accuracy)
 print("Confusion Matrix: ")
 print(cMatrix)
+
+numDataToDisplay = 5
+successResults = []
+failedResults = []
+testY = pd.Series.tolist(yTest)
+for i in range(0, len(yPred)):
+    if(yPred[i] == testY[i]):
+        successResults.append(scaler.inverse_transform(xTest[i]))
+    else:
+        failedResults.append(scaler.inverse_transform(xTest[i]))
+
+for i in range(0, len(successResults)):
+    print("Successful input #{}: {}".format(i+1, successResults[i]))
+    if(i + 1 == numDataToDisplay):
+        break
+
+for i in range(0, len(failedResults)):
+    print("Failed input #{}: {}".format(i+1, failedResults[i]))
+    if(i + 1 == numDataToDisplay):
+        break
+
 
